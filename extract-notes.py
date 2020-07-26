5149d1d845b86b9b2db632d071c4a05ff27c6fa4
Christoph <christoph.tittel@gmail.com>
2020-07-27 01:45:34 +0200

    added note extraction  part

--- extract-notes.py
+++ extract-notes.py
@@ -1,22 +1,45 @@
 import os
+import re
 
 d = {}
-
+matcher = re.compile(r'^(.*) +\((.*)\)$')
+# \\(([^\\(]*)\\)$
 def main():
-    with open("My Clippings.txt") as f:
+    with open("My Clippings.txt", encoding='utf-8-sig') as f:
         s = f.read()
 
     annotations = s.split("\n==========\n")
 
     for a in annotations:
-        add_to_dict(a)
+        add_annotation(a)
+    print(d)
 
-def add_to_dict(annotation):
+def add_annotation(annotation):
     lines = annotation.split('\n')
+    # print(lines)
 
-    if len(lines) >= 3:
-        print(lines)
-    
+    if len(lines) == 4:
+        title_author = extract_title_author(lines[0])
+        
+        if title_author not in d:
+            d[title_author] = [lines[3]]
+        else:
+            d[title_author].append(lines[3])
+    elif len(lines) > 4:
+        raise "ERROR: There should always be 4 lines"
+
+def extract_title_author(first_line):
+    """ Takes the first line of an annotation
+        Returns formatted title and author (which is then used as filename) """
+
+    # print(first_line)
+    m = matcher.match(first_line)
+    # author = author.group(1)
+
+    title = m[1]
+    author = m[2]
+
+    return title + " - " + author
 
 if __name__ == "__main__":
     main()
