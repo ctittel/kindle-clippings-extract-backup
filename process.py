3eaefde430110bdf0b76d71655c23457af6127b2
Christoph <christoph.tittel@gmail.com>
2020-07-27 13:38:28 +0200

    json part working

--- process.py
+++ process.py
@@ -1,8 +1,13 @@
 import os
 import re
+import json
+from pathlib import Path
 
 matcher = re.compile(r'^(.*) +\((.*)\)$')
 annotations_dict = {}
+
+json_files_dir = Path("./json")
+notes_files_dir = Path("./notes")
 
 class BookData:
     def __init__(self, title, authors):
@@ -19,10 +24,16 @@ def main():
     with open("My Clippings.txt", encoding='utf-8-sig') as f:
         s = f.read()
 
+    # create used directories if they don't exist
+    json_files_dir.mkdir(exist_ok=True)
+    notes_files_dir.mkdir(exist_ok=True)
+
     annotations = s.split("\n==========\n")
     for a in annotations:
         add_annotation_to_dict(a)
-    write_annotations_to_files()
+
+    update_json_files()
+    write_notes_files()
 
 def add_annotation_to_dict(annotation):
     """ Takes an annotation block (between the == in the My Clippings file)
@@ -45,14 +56,31 @@ def add_annotation_to_dict(annotation):
     elif len(lines) > 4:
         raise "ERROR: There should always be 4 lines"
 
-def write_annotations_to_files():
-    print(annotations_dict)
-    for bd, annotations in annotations_dict.items():
-        print("item")
-        # Build filename of output files
-        fn = bd.title + " - " + ", ".join(bd.authors) + ".md"
+def make_json_file_path(bd: BookData):
+    return Path(json_files_dir, bd.title + " - " + ", ".join(bd.authors) + ".json")
 
-        with open(fn, 'w', encoding='utf-8') as f:
+def make_note_file_path(bd: BookData):
+    return Path(notes_files_dir, bd.title + " - " + ", ".join(bd.authors) + ".md")
+
+def update_json_files():
+    """ Create json files for each book if it not exists.
+        Otherwise merge the new stuff into the existing json file"""
+
+    for bd, new_notes in annotations_dict.items():
+        with open(make_json_file_name(bd), 'w+') as f:
+            file_content = f.read()
+            notes = []
+            if file_content:
+                notes = json.loads()['notes']
+            notes_set = set(notes)
+            for nn in new_notes:
+                if nn not in notes_set:
+                    notes.append(nn)
+            f.write(json.dumps({'notes':notes}))
+
+def write_notes_files():
+    for bd, annotations in annotations_dict.items():
+        with open(make_note_file_path(bd), 'w', encoding='utf-8') as f:
             content = ""
 
             # Write the book title in the first line
